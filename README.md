9ae43c543a659a9fae65af3206c3d3633097d2d3
Zyanik <gerardoperez8a@gmail.com>
2023-06-09 22:48:34 -0700

    Finished up styling and commenting

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Submitted by: Gerardo Perez
 
-Estimated time spent: **#** hours spent in total
+Estimated time spent: 6 hours spent in total
 
 Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
@@ -10,25 +10,25 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
 #### Core Features
 
-- [ ] User can view a list of current movies from The Movie Database API as a grid view
+- [X] User can view a list of current movies from The Movie Database API as a grid view
   - The grid element should have an id of `movies-grid`
   - Each movie wrapper element should have a class of `movie-card`
-- [ ] For each movie displayed, user can see the following details:
+- [X] For each movie displayed, user can see the following details:
   - Title - the element should have a class of `movie-title`
   - Image - the `img` element should have a class of `movie-poster`
   - Votes - the element should have a class of `movie-votes`
-- [ ] User can load more current movies by clicking a button at the bottom of the list
+- [X] User can load more current movies by clicking a button at the bottom of the list
   - The button should have an id of `load-more-movies-btn`.
   - When clicked, the page should not refresh.
   - New movies should simply be added to the bottom
-- [ ] Allow users to search for movies and display them in a grid view
+- [X] Allow users to search for movies and display them in a grid view
   - There should be a search input element with an id of `search-input`
   - Users should be able to type into the input
   - When a user hits 'Enter', it should send a search request to the movies API
   - The results from the search should be displayed on the page
   - There should be a close icon with an id of `close-search-btn` that exits the search, clears results, and shows the current movies displayed previously
-- [ ] Website accounts for basic HTML/CSS accessibility features
-- [ ] Website should be responsive
+- [X] Website accounts for basic HTML/CSS accessibility features
+- [X] Website should be responsive
 
 #### Stretch Features
 
@@ -40,23 +40,22 @@ Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
 
 ### Walkthrough Video
 
-`TODO://` Add the embedded URL code to your animated app walkthrough below, `ADD_EMBEDDED_CODE_HERE`. Make sure the video or gif actually renders and animates when viewing this README. (🚫 Remove this paragraph after adding walkthrough video)
+'<div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/b5e0ab4a2e4a459da8d4878920b1cd8f" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>'
 
-`ADD_EMBEDDED_CODE_HERE`
-
+URL if needed: https://www.loom.com/share/b5e0ab4a2e4a459da8d4878920b1cd8f
 ### Reflection
 
 - Did the topics discussed in your labs prepare you to complete the assignment? Be specific, which features in your weekly assignment did you feel unprepared to complete?
 
-Add your response here
+Overall, i owuld say so. The overall dea f starting from scratch was very scray and difficult to work around. I think documentation wlakthrough would be very beneficial. I had a hard time trying to figure out the appropriate way to call the API , so just advice on how to use resources
 
 - If you had more time, what would you have done differently? Would you have added additional features? Changed the way your project responded to a particular event, etc.
   
-Add your response here
+I would have definitely added more things. One of them being a pop up that shows the video and description, i would ahve also added a simpleline of text that says "Now Playing" and 'Showing Results for: SearchQuery" and other small things like that. Definitely more animation for buttons and overall background decoration.
 
 - Reflect on your project demo, what went well? Were there things that maybe didn't go as planned? Did you notice something that your peer did that you would like to try next time?
 
-Add your response here
+I think everything really went well. Something I would like to implement that i saw my peer implemented would be a pop up
 
 ### Open-source libraries used
 
@@ -65,3 +64,5 @@ Add your response here
 ### Shout out
 
 Give a shout out to somebody from your cohort that especially helped you during your project. This can be a fellow peer, instructor, TA, mentor, etc.
+
+I would like to give a shout out to everyone in my cohort. They have truly made me feel comfortable in class and felt confident in the learning proccess.
--- script.js
+++ script.js
@@ -17,6 +17,8 @@ const loadMoreButton = document.getElementById('load-more-movies-btn')
 
 const flixterTitle = document.getElementById('title')
 
+const movieList = document.getElementById('movie-grid')
+
 // this function clls the API for it's general 
 const trendingAccesser = async () => {
     // increment PageNumber to account for the load more
@@ -29,128 +31,171 @@ const trendingAccesser = async () => {
     const res = await fetch(url)
     const data = await res.json()
     
-    data.results.forEach((item) => {
-        movieAdder(item)
+    // we run the movieAdder function on every movieObject we get in our 
+    // array of movieobjects
+    data.results.forEach((movieObject) => {
+        movieAdder(movieObject)
     })
 }
 
 
-
+// this function calls the API based on a search query the user makes
 const searchAccesser = async (searchString) => {
+    // we increment the pageNumber on every call to account for the set of movies we
+    // want to display when we press load more
     PageNumber += 1
     
+    // we create the url using the page numbr and what the user wanted to search
     const url = `https://api.themoviedb.org/3/search/movie?api_key=b561f2ff1a96972581e6a5fff508215e&query=${searchString}&include_adult=false&language=en-US&page=${PageNumber}`
 
+    // retrieve what the URL gives us
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
-
-    console.log(data.results)
-    data.results.forEach((item) => {
-        movieAdder(item)
+    
+    // we run the movieAdder function on every movieObject
+    data.results.forEach((movieObject) => {
+        movieAdder(movieObject)
     })
 
 }
 
-const movieList = document.getElementById('movie-grid')
-
+// this resets our page to the original display of trending movies
 const pageReset = () => {
+    //we reset the PageNumber to 0
     PageNumber = 0
 
+    // we clear the searchQuery
     searchQuery = ''
     
+    // we clear the movie cards
     movieList.innerHTML = ''
 
+    // we return back to trending movies
     trending = true
 
     search = false
+
+    
 }
 
+
+// this function takes in a movieObject and extracts the title, rating, and
+// poster to then add it to our HTML
 const movieAdder = (movieObject) => {
     // create our div that distinguishes a movie card
     const movie = document.createElement('div')
 
+    // give it the appropriate class
     movie.setAttribute('class', 'movie-card')
 
-    // we get our image url from the movieObject and add it 
-    // to our movie-card
+    // we get our image path from our movieObjec
 
-    let movieURL = ''
+    //our posterURL is intialzed as empty
+    let posterURL = ''
 
+    // if our posterPath is empty, we default to a no-preview image
     if (movieObject.poster_path == null){
-        movieURL = 'no-preview.png'
+        posterURL = 'no-preview.png'
     }
 
+    // if there is a path, we create a URL that we will use to retrieve
+    // the poster image
     else{
-        movieURL =  `https://image.tmdb.org/t/p/w500/${movieObject.poster_path}`
+        posterURL =  `https://image.tmdb.org/t/p/w500/${movieObject.poster_path}`
     }
-
-    ``
-
-    const moviePoster = document.createElement('img')
     
-    // we create the img element with the poseter url
-    // and add the neccessary class name
-    moviePoster.setAttribute('src', movieURL)
+    // we create the img element with the poster URL and add the neccessary class name
+    const moviePoster = document.createElement('img')
+
+    moviePoster.setAttribute('src', posterURL)
+    
     moviePoster.setAttribute('class', 'movie-poster')
 
-    movie.appendChild(moviePoster)
-    
+    // we create a h3 elememnt for our movieTitle and add the title string
+    // from our movieObject to it and add the appropriate class to it
     const movieTitle = document.createElement('h3')
 
     movieTitle.innerHTML = movieObject.title
 
     movieTitle.setAttribute('class', 'movie-title')
 
+    // we create a h4 elememnt for our movieTitle and add the review string
+    // from our movieObject to it and add the appropriate class to it
     const movieRating = document.createElement('h4')
 
     movieRating.innerHTML = `⭐ ${movieObject.vote_average}`
 
     movieRating.setAttribute('class', 'movie-rating')
 
+    // we append the img, h3, and h4 element to the movie-card div we created
+    movie.appendChild(moviePoster)
+
     movie.appendChild(movieTitle)
     
     movie.appendChild(movieRating)
 
+    // add the card to the movie-grid we have in our HTML
     movieList.appendChild(movie)
 }
 
+
+// this function add the appropriate listeners to all the required elements in our HTML
 const addEventListeners = (loadMore, searchSubmit, titleReset) => {
+    
+    // this adds an event listener to our loadMore function that calls the API
     loadMore.addEventListener ('click', () => {
+
+        //if trending is true, we call the trending API when load more is pressed
         if (trending){
             trendingAccesser()
         }
+
+        // if search is true, we call the API with the query
         else if (search){
             searchAccesser(searchQuery)
         }
     })
 
-    searchSubmit.addEventListener('keypress', function (e) {
+    // this adds an enter key listener to our search bar that will take the
+    // inputted text and do a searc queryusing it
+    searchSubmit.addEventListener('keypress', (e) => {
 
+        // this detects that the key is enter
         if (e.key === 'Enter') {
+            //prevents the page rom automatically reloading
             e.preventDefault()
-
+            
+            // resets the page
             pageReset()
-
+            
+            // changes our booleans to note we are now searching
             search = true
     
             trending = false
             
+            // gets our search query from our input element
             searchQuery = document.getElementById('search-input').value
-
-            console.log(searchQuery)
             
+            // deletes the user input from the search bar
+            document.getElementById('search-input').value = ''
+
+            //console.log(searchQuery)
+            
+            // if the user didn't input anything, just show trending
             if (searchQuery == ''){
                 pageReset()
     
                 trendingAccesser()
             }
+
+            // if the user dd input something, call a search query on their input
             else{
                 searchAccesser(searchQuery)
             }
         }
     })   
 
+    // this listener resets the page when the user clicks on the Flixer title
     titleReset.addEventListener('click', () => {
         pageReset()
 
@@ -158,6 +203,7 @@ const addEventListeners = (loadMore, searchSubmit, titleReset) => {
     })
 }
 
+// this function loads uur trending movies and adds our event listeners when the page is loaded
 window.onload = () => {
     trendingAccesser()
     addEventListeners(loadMoreButton, searchSubmit, flixterTitle)
